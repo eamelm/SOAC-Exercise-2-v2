d423d0406f6a0168614a2121dfc0def6092a20da
eamelm <71378917+eamelm@users.noreply.github.com>
2020-10-05 14:47:13 +0200

    New python code

---
+++ Exercise2_v1.py
@@ -0,0 +1,33 @@
+"""
+SOAC Exercise 2: Molenkamp test
+Perform Molenkamp simulation using three different numerical schemes:
+    (1) Euler forward in time and upwind in space
+    (2) "Lax-Wendroff" scheme
+    (3) The spectral method
+
+T. Bovenschen
+E.A. Melman
+@ Utrecht University
+"""
+
+# Import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# --------------------------------------------
+# INITIALIZATION
+# --------------------------------------------
+
+# --------------------------------------------
+# (1) Euler forward in time and upwind in space
+# --------------------------------------------
+
+# --------------------------------------------
+# (2) "Lax-Wendroff" scheme
+# --------------------------------------------
+
+# --------------------------------------------
+# (3) The spectral method
+# --------------------------------------------
+
