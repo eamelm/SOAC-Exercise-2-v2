61dc65af8bb6b378333e538afe6d1cdccdb577bc
eamelm <71378917+eamelm@users.noreply.github.com>
2020-10-05 15:13:33 +0200

    new

--- Exercise2_v1.py
+++ Exercise2_v1.py
@@ -23,15 +23,22 @@ L = 2500 # [km]
 Dx = 25 # Gridsize [km]
 x = np.arange(0, L, Dx)
 
-# Define concentration
-C0 = 1 # Initial concentration
-C = np.zeros(len(x))
-C[46:55] = C0
+# Define time array
+t = np.arange(0,100)
+
+# Define initial concentration
+C = np.zeros((len(t), len(x)))
+C[46:55] = 1 # Initial concentration
+
+# Define initial wind speed
+u0 = 10 # Initial wind speed [m/s]
 
 # --------------------------------------------
 # (1) Euler forward in time and upwind in space
 # --------------------------------------------
-
+for nt in range(len(t)):
+    for nx in range(len(x)):
+        C[nx, nt+1] - 
 
 # --------------------------------------------
 # (2) "Lax-Wendroff" scheme
