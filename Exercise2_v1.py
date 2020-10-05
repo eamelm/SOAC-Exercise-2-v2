08eef39b1b8eb9d62abaf8675b2c17e32cda41b7
eamelm <71378917+eamelm@users.noreply.github.com>
2020-10-05 15:00:10 +0200

    init defined

--- Exercise2_v1.py
+++ Exercise2_v1.py
@@ -18,14 +18,25 @@ import pandas as pd
 # --------------------------------------------
 # INITIALIZATION
 # --------------------------------------------
-2222
+# Define domain:
+L = 2500 # [km]
+Dx = 25 # Gridsize [km]
+x = np.arange(0, L, Dx)
+
+# Define concentration
+C0 = 1 # Initial concentration
+C = np.zeros(len(x))
+C[46:55] = C0
+
 # --------------------------------------------
 # (1) Euler forward in time and upwind in space
 # --------------------------------------------
 
+
 # --------------------------------------------
 # (2) "Lax-Wendroff" scheme
 # --------------------------------------------
+
 
 # --------------------------------------------
 # (3) The spectral method
