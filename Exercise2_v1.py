f6c89cd74787bd38a1e4108914beefc8af90f5ab
TBovenschen <71378919+TBovenschen@users.noreply.github.com>
2020-10-05 16:10:59 +0200

    Update Exercise2_v1.py

--- Exercise2_v1.py
+++ Exercise2_v1.py
@@ -27,29 +27,31 @@ Dx = 25 # Gridsize [km]
 x = np.arange(0, L, Dx)
 
 # Define time array
-t = np.arange(0,100)
 Dt = 0.1
+t = np.arange(0,100,Dt)
+
 # Define initial concentration
 C = np.zeros((len(t), len(x)))
 C[0,46:55] = 1 # Initial concentration
-
+C_lw =C
 # Define initial wind speed
 u0 = 10 # Initial wind speed [m/s]
 
 # --------------------------------------------
 # (1) Euler forward in time and upwind in space
 # --------------------------------------------
-for nt in range(len(t)-1):
-    for nx in range(1,len(x)):
+for nt in range(len(t)-1): #loop over time
+    for nx in range(1,len(x)):  #loop over x
         C[nt+1, nx] = -u0*(C[nt,nx]-C[nt,nx-1])/ Dx * Dt+ C[nt,nx]
 
-plt.figure()
-plt.plot(x,C[50,:])
-plt.show()
+
 # --------------------------------------------
 # (2) "Lax-Wendroff" scheme
 # --------------------------------------------
-
+for nt in range(len(t)-1):
+    for nx in range(1,len(x)-1):
+        C_lw[nt+1, nx] = (-u0*(C_lw[nt,nx+1]-C_lw[nt,nx-1])/ (2*Dx) + 
+        (u0**2 * Dt/2*(C_lw[nt,nx+1]-2*C_lw[nt,nx]+C_lw[nt,nx-1])/Dx**2)) * Dt+ C_lw[nt,nx]
 
 # --------------------------------------------
 # (3) The spectral method
@@ -67,9 +69,9 @@ def init(): #function for initial frame (empty)
     return line, 
 
 def anim(i): #function for the time evolution
-    line.set_data(x, C[i,:])
+    line.set_data(x, C_lw[i,:])
     return line, 
 
 #ax.set_title('$\Phi$ for timestep {:.2f} '.format(t)+'$\Lambda$={:.1f} and '.format(Lambda)+'$r$={:.4f}'.format(r))
-animat = animation.FuncAnimation(fig,anim, init_func=init, interval=1, blit=True)
+animat = animation.FuncAnimation(fig,anim, init_func=init, interval=10, blit=True)
 
